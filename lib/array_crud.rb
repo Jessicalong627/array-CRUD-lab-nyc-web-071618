8d7b175bdd6069106943e3ef9d559736b8caec67
Jessica Long <jessicalong627@gmail.com>
2018-07-03 18:10:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
- 
+  animals = ["bird", "giraffe","dog", "cat"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("element")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  animals.unshift("bear")
+  array.unshift("bear")
 end
 
 def remove_element_from_end_of_array(array)
-  animals.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  animals.push()
+  array.push()
 end
 
 def retrieve_element_from_index(array, index_number)
-  animals[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  animals[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-   animals[-1]
+   array[-1]
 end
